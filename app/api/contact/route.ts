035a2fc43df55dba476c231c13b2eaf4153171ad
KW920 <wessel-wolfgang@t-online.de>
2026-05-30 17:58:39 +0200

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
 
     const data = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "info@wessel-elektrotechnik.de",
+      to: "wessel-wolfgang@t-online.de",
       subject: "Neue Kontaktanfrage",
       html: `
         <h2>Neue Kontaktanfrage</h2>
